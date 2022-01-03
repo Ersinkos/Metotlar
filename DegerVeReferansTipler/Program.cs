96a949d9087e75b3919c6907ae9d32bafbbc8f49
ersinkos <ersinkoseoglu38@gmail.com>
2022-01-04 01:49:18 +0300

    Engin Demiroğ - Yazılım Geliştirici Yetiştirme Kampı (C# + Angular) 3.gün

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -17,6 +17,8 @@ namespace DegerVeReferansTipler
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
             Console.WriteLine(sayilar1[0]);//sayilar1[0] 100 degil 999 dur.
+
+
         }
     }
 }
