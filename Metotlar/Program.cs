80ff8e179955e1aa66e85ce26d2f32a3920c3306
mgame <mgame@DESKTOP-O311A3F>
2022-01-04 01:43:41 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyabakir Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            Console.WriteLine("------------------");
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------------");
+            }
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.ekle(urun1);
+            sepetManager.ekle(urun2);
+
+            sepetManager.ekle2("Armut", "Deveci Armutu", 15, 10);
+            sepetManager.ekle2("Elma", "Yesil Elma", 12, 7);
+            sepetManager.ekle2("Karpuz", "Diyarbakir Karpuzu", 50, 5);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+       public void ekle(Urun urun)
+       {
+            Console.WriteLine("Tebrikler. Sepete Eklendi! : " + urun.Adi);
+       }
+       public void ekle2(string urunAdi,string aciklama,double fiyati,int stokAdedi)
+       {
+            Console.WriteLine("Tebrikler. Sepete Eklendi! : " + urunAdi);
+       }
+    }
+}
